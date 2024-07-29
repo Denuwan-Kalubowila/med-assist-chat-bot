3ba73c4eae8c69f2075ce44c43eff1d85399d15c
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-07-30 01:09:37 +0530

    setup to google-gen-ai

--- src/chain.py
+++ src/chain.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import GoogleGenerativeAI,HarmBlockThreshold, HarmCategory,GoogleGenerativeAIEmbeddings
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from med_assist_retriever import med_assist_retriver_pinecone_db,med_assist_retriver_chroma_db
@@ -8,12 +9,19 @@ from prompt import custom_prompt_template
 
 load_dotenv()
 
-openai_api_key = os.getenv('OPENAI_API_KEY')
-os.environ['OPENAI_API_KEY'] = openai_api_key
+# openai_api_key = os.getenv('OPENAI_API_KEY')
+# os.environ['OPENAI_API_KEY'] = openai_api_key
+google_api_key = os.getenv('GOOGLE_API_KEY')
+os.environ['GOOGLE_API_KEY'] = google_api_key
 
-model = ChatOpenAI(
-    model='gpt-4',
-    temperature=0.6
+model = GoogleGenerativeAI(
+    model='gemini-pro',
+    temperature=0.6,
+    google_api_key=google_api_key,
+    verbose=True,
+    safety_settings={
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+    }
 )
 
 output_parser = StrOutputParser()
@@ -34,6 +42,4 @@ def med_QA(ques):
     )
     return qa_chain.invoke(input=ques)
 
-ans = med_QA(" What are the treatments for pneumonia")
-print(ans)
 
---
+++ src/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client =TestClient(app)
+
+def test_question_main():
+    payload = {
+        "question": "What is the treatment for a yellow fever?"
+    }
+    res = client.post("/questions", json=payload)
+    assert res.status_code == 200
+    
