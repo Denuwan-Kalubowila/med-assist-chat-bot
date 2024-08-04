b499c23475ca42b24c27bf84b016f418164d1774
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-08-04 23:41:29 +0530

    chat history

--- src/chain.py
+++ src/chain.py
@@ -1,12 +1,19 @@
 from dotenv import load_dotenv
 import os
-from langchain_openai import ChatOpenAI
-from langchain_google_genai import HarmBlockThreshold, HarmCategory, ChatGoogleGenerativeAI
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_google_vertexai import ChatVertexAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from med_assist_retriever import med_assist_retriver_pinecone_db, med_assist_retriver_chroma_db
-from prompt import custom_prompt_template
+from prompt import custom_prompt_template, history_chat_template
+
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+import secrets
+import string
 
 load_dotenv()
 
@@ -23,19 +30,50 @@ med_template = custom_prompt_template()
 # Initialize the retriever database
 retriever_db = med_assist_retriver_pinecone_db()
 
-def med_QA(ques):
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+def generate_session_id(length=5):
+    characters = string.ascii_uppercase + string.digits
+    session_id = ''.join(secrets.choice(characters) for _ in range(length))
+    return session_id
+
+def med_QA(ques, session_id=None):
+    if session_id is None:
+        session_id = generate_session_id()
+
     # Define the QA chain
-    qa_chain = (
-        {
-            "context": retriever_db.as_retriever(),
-            "question": RunnablePassthrough(),
-        } 
-        | med_template
-        | model 
-        | output_parser
+    qa_chain = create_stuff_documents_chain(
+        llm=model,
+        prompt=med_template
     )
-    
-    # Invoke the QA chain with the input question
-    return qa_chain.invoke(input=ques)
 
+    history_retriever = create_history_aware_retriever(
+        llm=model,
+        retriever=retriever_db.as_retriever(),
+        prompt=history_chat_template(),
+    )
 
+    rag_chain = create_retrieval_chain(history_retriever, qa_chain)
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="question",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+
+    result = conversational_rag_chain.invoke(
+        input={"question": ques, "chat_history": get_session_history(session_id).messages},
+        config=session_id
+    )
+
+    # Store the updated chat history
+    store[session_id] = get_session_history(session_id)
+
+    return result, session_id
--- src/prompt.py
+++ src/prompt.py
@@ -1,34 +1,65 @@
-from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate,HumanMessagePromptTemplate,PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder, PromptTemplate
 
-def custom_prompt_template ():
+def custom_prompt_template():
     template_str = """You are a medical assistant with expert knowledge in common medical problems faced by patients in day-to-day life. 
-                    Your task is to provide accurate, reliable, and responsible answers to solve problems faced by patients. 
-                    Use the given context to ensure accuracy in your answers. 
-                    If you are unsure about an answer, state that you don't know. 
-                    Your answers should be in an ordered list, short and concise.
-                    {context}
-            """
+Your task is to provide accurate, reliable, and responsible answers to solve problems faced by patients. 
+Use the given context to ensure accuracy in your answers. 
+If you are unsure about an answer, state that you don't know. 
+Your answers should be in an ordered list, short and concise.
+{context}
+"""
 
     system_prompt = SystemMessagePromptTemplate(
         prompt=PromptTemplate(
-            input_variables=["context"],template=template_str
+            input_variables=["context"],
+            template=template_str
         )
     )
 
     human_prompt = HumanMessagePromptTemplate(
         prompt=PromptTemplate(
-            input_variables=["question"],template="{question}"
+            input_variables=["question"],
+            template="{question}"
         )
     )
 
-    messages = [system_prompt,human_prompt]
-
-    custom_template = ChatPromptTemplate(
-        input_variables=["context","question"],
-        messages=messages,
+    custom_template = ChatPromptTemplate.from_messages(
+        [
+            system_prompt,
+            MessagesPlaceholder("chat_history"),
+            human_prompt,
+        ]
     )
 
     return custom_template
 
+def history_chat_template():
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+that can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
 
+    system_prompt = SystemMessagePromptTemplate(
+        prompt=PromptTemplate(
+            input_variables=["context"],
+            template=contextualize_q_system_prompt
+        )
+    )
+
+    human_prompt = HumanMessagePromptTemplate(
+        prompt=PromptTemplate(
+            input_variables=["question", "chat_history"],  # Include both "question" and "chat_history"
+            template="{question}"
+        )
+    )
+
+    custom_history_template = ChatPromptTemplate.from_messages(
+        [
+            system_prompt,
+            MessagesPlaceholder("chat_history"),
+            human_prompt,
+        ]
+    )
+
+    return custom_history_template
 
